9ff388591c19144532398460231f0b5b5a54a3db
Nicolai Frost <Nja@rehfeld.dk>
2016-04-07 21:25:07 +0200

    cash point scrape update

--- cashpoint.py
+++ cashpoint.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import html5lib
 import urllib
+
 siteName = "cashpoint"
 html_doc = urllib.urlopen('https://www.cashpoint.dk/da/site/index.html').read()
 soup = BeautifulSoup(html_doc, 'html.parser')
@@ -12,17 +13,23 @@ matchDict = {}
 for i in matches:
     homeTeam = i.find("div", { "class" : "live_group1" }).text.strip()
     awayTeam = i.find("div", { "class" : "live_group2" }).text.strip()
-    matchName = homeTeam+" - "+awayTeam  
+    matchName = homeTeam+" - "+awayTeam
     
-    test = i.findAll("div", { "class" : "live_td live_odds_container" })
+    listOfOdds = i.findAll("div", { "class" : "live_td live_odds_container" })
     counter = 0
     oddsList = []
-    for j in test:
-        oddsList.append(j.text.strip())
+    for j in listOfOdds:
+        odds = j.text.strip().replace(",",".")
+        try:
+            odds = float(odds)
+        except:
+            continue
+        oddsList.append(odds)
         counter +=1
         if counter == 3:
             break
 
     matchDict[matchName] = [oddsList,siteName]
         
-        
+
+
